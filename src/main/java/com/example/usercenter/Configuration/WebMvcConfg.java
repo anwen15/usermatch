620d675fbb2f52c33f068234023e79e90c03a967
fanfan <2876709381@qq.com>
2023-11-12 23:09:40 +0800

    解决跨域问题

---
+++ src/main/java/com/example/usercenter/Configuration/WebMvcConfg.java
@@ -0,0 +1,23 @@
+package com.example.usercenter.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfg implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //设置允许跨域的路径
+        registry.addMapping("/**")
+                //设置允许跨域请求的域名
+                .allowedOrigins("http://124.223.161.244:80","http://124.223.161.244:8000")
+                //是否允许证书 不再默认开启
+                .allowCredentials(true)
+                //设置允许的方法
+                .allowedMethods("*")
+                //跨域允许时间
+                .maxAge(3600);
+    }
+}

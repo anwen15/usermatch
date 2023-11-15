f7ba15a9397ce371fa4b1b210bcd7ac7efcd0ea0
fanfan <2876709381@qq.com>
2023-11-15 16:00:19 +0800

    解决跨域问题

--- src/main/java/com/example/usercenter/Configuration/WebMvcConfg.java
+++ src/main/java/com/example/usercenter/Configuration/WebMvcConfg.java
@@ -1,5 +1,6 @@
 package com.example.usercenter.Configuration;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,7 +13,8 @@ public class WebMvcConfg implements WebMvcConfigurer {
         //设置允许跨域的路径
         registry.addMapping("/**")
                 //设置允许跨域请求的域名
-                .allowedOrigins("http://124.223.161.244:80","http://124.223.161.244:8000")
+                //当**Credentials为true时，**Origin不能为星号，需为具体的ip地址【如果接口不带cookie,ip无需设成具体ip】
+                .allowedOrigins("http://124.223.161.244:80", "http://124.223.161.244:8000", "http://124.223.161.244:8082", "http://124.223.161.244:888","http://124.223.161.244")
                 //是否允许证书 不再默认开启
                 .allowCredentials(true)
                 //设置允许的方法
